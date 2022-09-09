ea011e3a6d9375223f864fb6190650cb4c5e99b4
Kevin Ihm <kevinihm@gmail.com>
2022-09-09 08:26:09 -0700

    removed test-requests.sh file

--- test-requests.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for (( c=1; c<=30; c++ ))
-do
-    curl http://localhost:6000/joke
-    curl http://localhost:6000/joke
-    curl http://localhost:6000/joke
-    curl http://localhost:6000/joke
-    curl http://localhost:6000/joke
-done

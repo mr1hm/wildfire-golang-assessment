44479ec87cb83a585772ab5d807239d190227e8a
Kevin Ihm <kevinihm@gmail.com>
2022-09-08 21:32:25 -0700

    added handlers_test.go to api and server packages

---
+++ internal/api/api_test/handlers_test.go
@@ -0,0 +1,3 @@
+package api_test
+
+// TODO: Implement unit/integrated tests for handlers.go in the api package
---
+++ internal/server/server_test/handlers_test.go
@@ -0,0 +1,3 @@
+package server_test
+
+// TODO: Implement unit/integrated tests for handlers in the server package
